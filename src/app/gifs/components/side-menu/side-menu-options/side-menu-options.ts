746359a9e9ec804d49b8e937d6116a7340b902fc
manromero <migu621@gmail.com>
2026-03-19 11:38:29 +0100

    side-menu-options

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
@@ -1,7 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  label: string;
+  subLabel: string;
+  router: string;
+  icon: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
   templateUrl: './side-menu-options.html',
+  imports: [RouterLink, RouterLinkActive],
 })
-export class SideMenuOptions {}
+export class SideMenuOptions {
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Treding',
+      subLabel: 'Popular Gifs',
+      router: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Search',
+      subLabel: 'Search Gifs',
+      router: '/dashboard/search',
+    },
+  ];
+}
